8cfdc9db780a88fb7ae202cab84a1d76bc982e18
Gabriel Campeão <campeao.275@gmail.com>
2023-03-01 23:53:29 -0300

    iniciando estudos de python para pentester

---
+++ Projeto1.py/teste.py
@@ -0,0 +1,10 @@
+nome= "Gabriel"
+idade= 30
+minha_lista= ["azul","amarelo","verdade",nome,idade]
+conjunto_cores = {'azul','verde','vermelho'}
+
+minha_lista.append('roxo')
+
+minha_lista.remove("amarelo")
+
+print(conjunto_cores)
